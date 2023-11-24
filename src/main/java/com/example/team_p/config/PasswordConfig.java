01096f96e1882854d558f097428271ae3516d338
pangseon <chaosen0521@gmail.com>
2023-11-24 12:25:06 +0900

    로그인 회워간입 구현

--- src/main/java/com/example/team_p/config/PasswordConfig.java
+++ src/main/java/com/example/team_p/config/PasswordConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
-
+ //Bean 추가
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
